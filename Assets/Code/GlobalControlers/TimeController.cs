0a4af609a9598ef4215b69741bb41cf8916a27e4
james7132 <james.siqi.liu@gmail.com>
2019-06-20 21:05:18 -0700

    Add pausing

---
+++ Assets/Code/GlobalControlers/TimeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public static TimeController Instance { get; private set; }
+
+    public bool IsPaused { get; private set; }
+
+    [SerializeField]
+    KeyCode _pauseKey = KeyCode.Escape;
+
+    [SerializeField]
+    float _defaultTimeScale = 1.0f;
+
+    void Awake() 
+    {
+        Instance = this;
+        IsPaused = false;
+        Time.timeScale = _defaultTimeScale;
+    }
+
+    void Update()
+    { 
+        if (!Input.GetKeyDown(_pauseKey)) return;
+        IsPaused = !IsPaused;
+        Time.timeScale = IsPaused ? 0.0f : _defaultTimeScale;
+        Debug.Log($"Paused State: {IsPaused}");
+    }
+}
