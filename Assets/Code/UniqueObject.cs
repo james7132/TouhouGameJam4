b75d76bad2d856698cedf7e4b0000a250b899399
james7132 <james.siqi.liu@gmail.com>
2019-06-22 16:10:56 -0700

    Seperate out persistent GameController vs Level Controller

---
+++ Assets/Code/UniqueObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniqueObject : MonoBehaviour
+{
+    static HashSet<int> _existingObjects;
+
+    [SerializeField]
+    int _id;
+
+    void Awake() 
+    {
+        if (_existingObjects.Contains(_id)) 
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        _existingObjects.Add(_id);
+    }
+
+    void Reset() 
+    {
+        _id = new System.Random().Next();
+    }
+
+}
