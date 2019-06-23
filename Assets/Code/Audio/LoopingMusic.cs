c45d042fd89464be2cd021d1139e5021ec350f72
Brian Intile <brianintile@gmail.com>
2019-06-23 03:50:29 -0400

    Add music controller

---
+++ Assets/Code/Audio/LoopingMusic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingMusic : MonoBehaviour
+{
+    
+    [SerializeField]
+    private float loopStartTime;
+    [SerializeField]
+    private float loopEndTime;
+    [SerializeField]
+    private bool startAtLoopPoint;
+    [SerializeField]
+    private bool testLoopPoint;
+
+    private AudioSource _audioSource;
+
+	void Awake()
+	{
+        _audioSource = GetComponent<AudioSource>();
+        if (testLoopPoint)
+            _audioSource.time = loopEndTime - 5f;
+        else if (startAtLoopPoint)
+            _audioSource.time =  loopStartTime;
+    }
+	
+	void Update()
+	{
+        if (_audioSource.time >= loopEndTime)
+            _audioSource.time -= loopEndTime - loopStartTime;
+	}
+}
---
+++ Assets/Code/Audio/MusicController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public static MusicController instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance.GetComponent<AudioSource>().clip != GetComponent<AudioSource>().clip)
+        {
+            Destroy(instance);
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+
+    }
+}
