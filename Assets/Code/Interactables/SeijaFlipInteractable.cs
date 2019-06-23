28e4389a56c4a10aa0815395aedc40879e0233da
Brian Intile <brianintile@gmail.com>
2019-06-22 20:11:10 -0400

    Create SeijaFlip and ShinResize Interactable scripts

---
+++ Assets/Code/Interactables/SeijaFlipInteractable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeijaFlipInteractable : TriggerListener
+{
+    protected override void OnTriggerFired(TriggerBehaviour trigger, Character source)
+    {
+        GetComponent<Rigidbody2D>().gravityScale *= -1f;
+    }
+}
---
+++ Assets/Code/Interactables/ShinResizeInteractable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShinResizeInteractable : TriggerListener
+{
+    [SerializeField]
+    private int _sizeLevels = 2;
+    [SerializeField]
+    private int _currentSizeLevel = 0;
+    [SerializeField]
+    private float _sizeLevelUpMult = 2f;
+    [SerializeField]
+    private float _massLevelUpMult = 2f;
+
+    protected override void OnTriggerFired(TriggerBehaviour trigger, Character source)
+    {
+        var scale = transform.localScale;
+        var rb2d = GetComponent<Rigidbody2D>();
+        _currentSizeLevel++;
+        if (_currentSizeLevel < _sizeLevels)
+        {
+            scale *= _sizeLevelUpMult;
+            rb2d.mass *= _massLevelUpMult;
+        }
+        else
+        {
+            for (int i = 0; i < _sizeLevels; i++)
+            {
+                scale /= _sizeLevelUpMult;
+                rb2d.mass /= _massLevelUpMult;
+            }
+            _currentSizeLevel = 0;
+        }
+
+        transform.localScale = scale;
+    }
+}
